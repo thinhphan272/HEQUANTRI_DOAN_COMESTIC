fac857594bcf9b482c8ed69deeee043c91cd2d3c
Thinha1 <Thinhanhdang014@gmail.com>
2025-11-25 15:26:19 +0700

    done them xoa nhan vien in database

--- DoAnHQT_DATABASE/DoAnHQT_DATABASE/Areas/Admin/Service/AccountService.cs
+++ DoAnHQT_DATABASE/DoAnHQT_DATABASE/Areas/Admin/Service/AccountService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -8,7 +10,7 @@ namespace DoAnHQT_DATABASE.Areas.Admin.Service
 {
     public class AccountService
     {
-
+        string connectionString = ConfigurationManager.ConnectionStrings["QL_BANHANG_ONLINE"].ToString();
         public bool Connect(string username, string password, string role)
         {
             string connStr = $"Data Source=localhost;Initial Catalog=QL_BANHANG_ONLINE;User ID={username};Password={password};";
@@ -34,6 +36,89 @@ namespace DoAnHQT_DATABASE.Areas.Admin.Service
             {
                 // Xử lý lỗi kết nối, đăng nhập sai, hoặc lỗi thực thi query
                 Console.WriteLine($"Error during role check: {ex.Message}");
+                return false;
+            }
+        }
+
+        public List<string> GetAllStaffs()
+        {
+            List<string> lstNV = new List<string>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT UserName FROM dbo.F_GetAllUserInRole(@RoleName)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.AddWithValue("@RoleName", "Nhân viên");
+
+                        conn.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Đọc giá trị của cột "UserName" và thêm vào List
+                                // Đảm bảo cột UserName không bị null
+                                lstNV.Add(reader["UserName"].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi, ví dụ: ghi log hoặc throw lại lỗi
+                lstNV.Add("N/A");
+                return lstNV;
+            }
+            return lstNV;
+        }
+
+        public bool AddStaff(string username, string password)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string function = "P_ADD_DATABASEUSER";
+                    using (SqlCommand cmd = new SqlCommand(function, conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@USERNAME", username);
+                        cmd.Parameters.AddWithValue("@PASSWORD", password);
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Lỗi trong lúc xử lý thêm!");
+                return false;
+            }
+        }
+
+        public bool DeleteStaff(string username)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string function = "P_DELETE_DATABASEUSER";
+                    using (SqlCommand cmd = new SqlCommand(function, conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@USERNAME", username);
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi trong lúc xử lý xoá!");
                 return false;
             }
         }
